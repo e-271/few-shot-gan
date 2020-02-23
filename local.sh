6dcca5a940faa5ce6daa9dd346e9848fcf066576
esther <erobb@vt.edu>
2020-02-22 19:03:07 -0500

    update local

--- local.sh
+++ local.sh
@@ -3,9 +3,9 @@ c=$1
 n=$2
 a=0
 lr=0.0002
-rr='pickles/
+rr='pickles/eng-config-f-10M.pkl'
 rri=0
-ddir='/mnt/slow_ssd/erobb/stylegan2-adaptive/evals'
+ddir='./results' # changeme
 
-python run_training.py --num-gpus=2 --data-dir=./datasets --config=config-$c --dataset=kannada4K --total-kimg=1000 --max-images=$n --resume-pkl=$rr --resume-kimg=$rri --rho=$a --lrate-base=$lr --result-dir=
+python run_training.py --num-gpus=2 --data-dir=./datasets --config=config-$c --dataset=kannada4K --total-kimg=1000 --max-images=$n --resume-pkl=$rr --resume-kimg=$rri --rho=$a --lrate-base=$lr --result-dir=$ddir
 
