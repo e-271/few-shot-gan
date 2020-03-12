683871c34ab9148ecf6f2a5ce2fb984256be518b
Esther Anne Robb <erobb@nrlogin1.cluster>
2020-03-11 21:55:35 -0400

    update launch script

--- launch_training.sh
+++ launch_training.sh
@@ -1,15 +1,18 @@
 #!/bin/bash
 
 # Usage
-# ./launch_training.py 100 a
+# ./launch_training.py N model tx kimg dir pt idx
 
 N=$1
 model=$2 # r, t, s, a
-reg=$3
-lr=$4
-tx=$5
-kimg=$6
-dir=$7
+reg=0
+lr=0.002
+tx=$3
+kimg=$4
+dir=$5
+pt=$6
+i=$7
+
 
 if [[ $(hostname) == "jb"* ]]; # RTX
 then
@@ -17,10 +20,10 @@ then
 ddir='/work/erobb/datasets/'
 rdir="/work/erobb/results/$7"
 
-else
+else # CA or NR
 
 ddir='./datasets'
-rdir="./results/$7"
+rdir="/work/newriver/erobb/results/$7"
 
 fi
 
@@ -31,6 +34,14 @@ hostname
 echo $CUDA_VISIBLE_DEVICES
 source activate py3tf115
 
+
+echo $pt
+
+if [[ $pt == "" ]]
+then
+
+echo "automatically choosing pretrain"
+i=0
 
 if [[ $tx == "kannada"* ]]
 then
@@ -50,47 +61,52 @@ then
 pt='./pickles/cat-config-f.pkl'
 
 
-elif [[ $tx == "danbooru"* ]] | [[ $tx == "anime"* ]]
+elif [[ $tx == "danbooru"* ]] || [[ $tx == "anime"* ]] || [[ $tx == "rei"* ]] || [[ $tx == "obama"* ]]
 then
 pt='./pickles/ffhq-config-f.pkl'
 
 fi
+fi
+
+echo $pt
 
 
 
 
-rv=''
-rvi=0
-rt=$pt
-rti=0
-rs=$pt
-rsi=0
-rr=$pt
-rri=0
+#rv=''
+#rvi=0
+#rt=$pt
+#rti=0
+#rs=$pt
+#rsi=0
+#rr=$pt
+#rri=0
+
+#rt="/work/newriver/erobb/results/table3/dog25/00001-stylegan2-dog25-2gpu-config-f-25img-rho0.0E00-lr2.0E-04/network-snapshot-000143.pkl"
+#rti=143
+#rr="/work/newriver/erobb/results/table3/anime25/00015-stylegan2-anime25-2gpu-config-c-b-25img-rho0.0E00-lr2.0E-04-aug/network-snapshot-000102.pkl"
+#rri=102
+
 
 # Vanilla GAN baseline
 if [[ $model == "v" ]]
 then
-python run_training.py --num-gpus=2 --data-dir=$ddir --config=config-f --dataset=$tx --total-kimg=$kimg --max-images=$N --resume-pkl=$rv --resume-kimg=$rvi --lrate-base=$lr --result-dir=$rdir/$tx
-echo "vvv"
+python run_training.py --num-gpus=2 --data-dir=$ddir --config=config-f --dataset=$tx --total-kimg=$kimg --max-images=$N --resume-pkl=$pt --resume-kimg=$i --lrate-base=$lr --result-dir=$rdir/$tx
 
 # TGAN baseline
 elif [[ $model == "t" ]]
 then
-echo $rt $rti
-python run_training.py --num-gpus=2 --data-dir=$ddir --config=config-f --dataset=$tx --total-kimg=$kimg --max-images=$N --resume-pkl=$rt --resume-kimg=$rti --lrate-base=$lr --result-dir=$rdir/$tx
-echo "ttt"
+python run_training.py --num-gpus=2 --data-dir=$ddir --config=config-f --dataset=$tx --total-kimg=$kimg --max-images=$N --resume-pkl=$pt --resume-kimg=$i --lrate-base=$lr --result-dir=$rdir/$tx
 
 # Shift+scale baseline
 elif [[ $model == "s" ]]
 then
-python run_training.py --num-gpus=2 --data-dir=$ddir --config=config-a-gb --dataset=$tx --total-kimg=$kimg --max-images=$N --resume-pkl=$rs --resume-kimg=$rsi --rho=$reg --lrate-base=$lr --result-dir=$rdir/$tx
-echo "sss"
+python run_training.py --num-gpus=2 --data-dir=$ddir --config=config-a-gb --dataset=$tx --total-kimg=$kimg --max-images=$N --resume-pkl=$pt --resume-kimg=$i --rho=$reg --lrate-base=$lr --result-dir=$rdir/$tx
 
 # Residual adapters (ours#)
 elif [[ $model == "r" ]]
 then
-python run_training.py --num-gpus=2 --data-dir=$ddir --config=config-c-b --dataset=$tx --total-kimg=$kimg --max-images=$N --resume-pkl=$rr --resume-kimg=$rri --rho=$reg --lrate-base=$lr --result-dir=$rdir/$tx
-echo "rrr"
+python run_training.py --num-gpus=2 --data-dir=$ddir --config=config-c-b --dataset=$tx --total-kimg=$kimg --max-images=$N --resume-pkl=$pt --resume-kimg=$i --rho=$reg --lrate-base=$lr --result-dir=$rdir/$tx
 fi
 
+echo "done."
